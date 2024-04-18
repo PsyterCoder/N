50e12b0c5f6581502d9631071eba9856e5a24e7d
PsyterCoder <167316151+PsyterCoder@users.noreply.github.com>
2024-04-18 21:37:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 #print("Hello World")
 import streamlit as srt
 srt.title("App by PS")
+a=srt.number_input("Enter number a")
+b=srt.number_input("Enter number b")
+c=srt.number_input("Enter number c")
+
