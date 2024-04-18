f617e960a1422074bf09b93445a5c5c7deb6c6c1
PsyterCoder <167316151+PsyterCoder@users.noreply.github.com>
2024-04-18 23:03:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ a=srt.number_input("Enter number a")
 b=srt.number_input("Enter number b")
 c=srt.number_input("Enter number c")
 srt.write("The biggest number is "+str(max(a,b,c)))
-
+def text(text,col,bgcolor):
+  srt.markdown(f"<p style='background-color:{bgcolor}; color:{col}'>{text}</p>" unsafe_html_allow=True )
+text("Thank you for using my APP","#FFFFFF","#000000")
