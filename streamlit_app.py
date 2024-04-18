e11520842f2c29232a82043752f268860c0a90b9
PsyterCoder <167316151+PsyterCoder@users.noreply.github.com>
2024-04-18 21:40:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ srt.title("App by PS")
 a=srt.number_input("Enter number a")
 b=srt.number_input("Enter number b")
 c=srt.number_input("Enter number c")
+srt.write("The biggest number is "+str(max(a,b,c)))
 
