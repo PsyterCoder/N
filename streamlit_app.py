5c3d4b4576a8f38bd2fc21d3c79a1fa8c76ff9f1
PsyterCoder <167316151+PsyterCoder@users.noreply.github.com>
2024-04-18 23:16:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ srt.write("The biggest number is "+str(max(a,b,c)))
 def text(text,col,bgcolor):
   srt.markdown(f"<p style='background-color:#000000; color:#0000FF'>{text}</p>", unsafe_allow_html=True )
 text("Thank you for using my APP","#FFFFFF","#000000")
+def gradientText(Text):
+  srt.markdown(f"<p style='background-image:linear-gradient(Azure,RoyalBlue); color:#FFFFFF'>{Text}</p>", unsafe_allow_html=True )
+gradientText("We are glad to help you , visit again ")
